78293f6632d58f987b69cbe71886f92a2083e333
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-08-11 11:05:19 -0500

    Preview pages

--- OfferApp/lib/src/screens/Login/Widgets/Input_text.dart
+++ OfferApp/lib/src/screens/Login/Widgets/Input_text.dart
@@ -11,55 +11,35 @@ class _InputTextState extends State<InputText> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * 0.35,
-            left: 20,
-            right: 20),
-        child: Column(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                //color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                /*boxShadow: [
-                    BoxShadow(
-                        color: Color.fromRGBO(36, 108, 251, 8),
-                        blurRadius: 1.0,
-                        offset: Offset(0, 1)),
-                  ]*/
-              ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            bottom: BorderSide(color: Colors.grey[300]))),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Usuario o email",
-                          hintStyle: TextStyle(color: Colors.grey[500])),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            bottom: BorderSide(color: Colors.grey[300]))),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Contraseña",
-                          hintStyle: TextStyle(color: Colors.grey[500])),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ));
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey[300]))),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Usuario o email",
+                  hintStyle: TextStyle(color: Colors.grey[500])),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey[300]))),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Contraseña",
+                  hintStyle: TextStyle(color: Colors.grey[500])),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- OfferApp/lib/src/screens/Login/login.dart
+++ OfferApp/lib/src/screens/Login/login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'Widgets/Input_text.dart';
 import 'Widgets/Login-Button.dart';
 import 'Widgets/TryfferLogo.dart';
-import 'Widgets/city-background.dart';
 import 'Widgets/google-facebook-button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -15,69 +14,67 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            Positioned(bottom: 0, child: CityBackground()),
-            Positioned(
-                top: MediaQuery.of(context).size.height / 8,
-                left: MediaQuery.of(context).size.width / 3.5,
-                child: TryfferLogo(MediaQuery.of(context).size.height / 5,
-                    MediaQuery.of(context).size.width / 2.5)),
-            Positioned(
-              top: MediaQuery.of(context).size.height / 1.8,
-              left: MediaQuery.of(context).size.height * 0.07,
-              child: ButtonGreen(
-                text: "Login",
-                onPressed: () {
-                  Navigator.pushNamed(context, "home");
-                },
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
+            ),
+            TryfferLogo(MediaQuery.of(context).size.height / 5,
+                MediaQuery.of(context).size.width / 2.5),
+            SizedBox(
+              height: 30,
+            ),
+            InputText(),
+            ButtonGreen(
+              text: "Login",
+              onPressed: () {
+                Navigator.pushNamed(context, "home");
+              },
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.06,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 10),
                 width: MediaQuery.of(context).size.width * 0.7,
                 height: MediaQuery.of(context).size.height * 0.06,
-              ),
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height / 1.5,
-              left: MediaQuery.of(context).size.height * 0.07,
-              child: Container(
-                  margin: EdgeInsets.only(top: 10),
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  height: MediaQuery.of(context).size.height * 0.06,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(
-                            right: (MediaQuery.of(context).size.width * 0.7) *
-                                0.09),
-                        child: SocialNetworkButtonLogin(
-                          colors: Color.fromRGBO(4, 85, 157, 1),
-                          image: "assets/img/logos/logofacebook.png",
-                          onPressed: () {},
-                          width:
-                              (MediaQuery.of(context).size.width * 0.7) * 0.45,
-                          height: MediaQuery.of(context).size.height * 0.06,
-                        ),
-                      ),
-                      SocialNetworkButtonLogin(
-                        colors: Color.fromRGBO(227, 65, 51, 1),
-                        image: "assets/img/logos/logogoogle.png",
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                          right:
+                              (MediaQuery.of(context).size.width * 0.7) * 0.09),
+                      child: SocialNetworkButtonLogin(
+                        colors: Color.fromRGBO(4, 85, 157, 1),
+                        image: "assets/img/logos/logofacebook.png",
                         onPressed: () {},
                         width: (MediaQuery.of(context).size.width * 0.7) * 0.45,
                         height: MediaQuery.of(context).size.height * 0.06,
                       ),
-                    ],
-                  )),
+                    ),
+                    SocialNetworkButtonLogin(
+                      colors: Color.fromRGBO(227, 65, 51, 1),
+                      image: "assets/img/logos/logogoogle.png",
+                      onPressed: () {},
+                      width: (MediaQuery.of(context).size.width * 0.7) * 0.45,
+                      height: MediaQuery.of(context).size.height * 0.06,
+                    ),
+                  ],
+                )),
+            SizedBox(
+              height: 20,
             ),
-            InputText(),
-            Positioned(
-                top: MediaQuery.of(context).size.height * 0.75,
-                left: MediaQuery.of(context).size.width / 5,
-                child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "No tienes cuenta? Registrate aquí",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )))
+            TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "register");
+                },
+                child: Text(
+                  "No tienes cuenta? Registrate aquí",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
           ],
         ),
       ),
